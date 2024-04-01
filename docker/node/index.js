e128d06f88181dfb36e2884fdbdf8c2b239215ad
rodolfosouzamenezes <rodolfosouzaofc@gmail.com>
2024-04-01 04:17:00 -0300

    feat(docker): insert data into database with node

--- docker/node/index.js
+++ docker/node/index.js
@@ -1,7 +1,21 @@
 const express = require("express")
+const mysql = require("mysql")
 
 const app = express()
 const port = 3000
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+}
+
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Rodolfo')`
+connection.query(sql)
+connection.end()
+
 app.get('/', (req, res) => {
   res.send('<h1>NODEJS COM DOCKER</h1>')
 })
